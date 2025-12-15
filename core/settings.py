235c412a9866ece019ea3a3e478a3e6e64850a27
Darshh16 <darshjilka09@gmail.com>
2025-12-15 21:14:38 +0530

    Fix

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2a(3vvw6(f=@qgp#i1wesdugc0%#6-_j^sx*e0zh-zu!cwcq8i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
